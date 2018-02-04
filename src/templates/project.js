f089599ec355ee3826faa77a90585018f6c000b5
Jon Bukiewicz <jonathan@tenebroso.net>
2018-02-03 21:01:39 -0600

    Additional WIP styling and setup for projects

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
+import styles from "./project.module.css"
 
 class ProjectTemplate extends Component {
   render() {
@@ -7,8 +8,30 @@ class ProjectTemplate extends Component {
 
     return (
       <div>
-        <h1 dangerouslySetInnerHTML={{ __html: project.title }} />
-        <div dangerouslySetInnerHTML={{ __html: project.content }} />
+        <div className={styles.header} style={{
+                backgroundImage: 'url(' + project.featured_media.source_url + ')'
+            }}>
+            <div className={styles.headerTitle}>
+                <h1 className={styles.title} dangerouslySetInnerHTML={{ __html: project.title }} />
+                <br />
+                <span className={styles.description} dangerouslySetInnerHTML={{__html: project.acf.subtitle}} />
+            </div>
+        </div>
+        <div className={styles.content}>
+            <div dangerouslySetInnerHTML={{ __html: project.acf.short_description }} />
+
+            <div dangerouslySetInnerHTML={{ __html: project.acf.highlights }} />
+
+            <div dangerouslySetInnerHTML={{ __html: project.acf.services }} />
+
+            <div>
+                <img src={project.acf.mobile_image.source_url} />
+            </div>
+
+            <div>
+                <img src={project.acf.laptop_image.source_url} />
+            </div>
+        </div>
       </div>
     )
   }
@@ -24,9 +47,24 @@ export default ProjectTemplate
 
 export const pageQuery = graphql`
   query currentProjectsQuery($id: String!) {
-    wordpressWpProjects(id: { eq: $id }) {
+    wordpressWpProjects(id: {eq: $id}) {
       title
       content
+      featured_media {
+          source_url
+      }
+      acf {
+        subtitle
+        short_description
+        highlights
+        services
+        mobile_image {
+         source_url
+        }
+        laptop_image {
+         source_url
+        }
+      }
     }
   }
 `
