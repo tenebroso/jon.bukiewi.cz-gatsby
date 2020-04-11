e567e8b04d42521069b62703efd700aa8b525bcc
Jon Bukiewicz <jonathan@tenebroso.net>
2020-04-11 17:52:15 -0500

    Copy updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <p>Hey, there. Thanks for stopping by!</p>
-    <p>My name is Jon Bukiewicz and I grew up building things on the web. I strive to find solutions by leveraging over 20 years of experience in front end development, project management, user-centric design, and clear communication.</p>
-    <p>I currently work for a <a href="https://www.gorillagroup.com/" target="_blank" rel="noopener noreferrer">digital agency</a> out of Chicago, building complex e-commerce experiences on a variety of platforms. In addition, I have multiple years of experience in front end frameworks like React, Vue, and Angular, as well as utilizing test driven development through Jest, Enzyme and Cypress.</p>
-    <p>Outside of my day job, I have built and maintained websites for a variety of clients for the past 8 years. From securing hosting, to designing websites, handling full stack development and helping clients better understand their platforms, my freelance work speaks for itself. I primarily have focused on WordPress, but have also had the opportunity to dabble in Angular, ReactJs and more.</p>
+    <p>My name is Jon Bukiewicz and I grew up building things on the web.</p>
+    <p>I strive to find solutions by leveraging over 20 years of experience in front end development, project management, user-centric design, and clear communication.</p>
+    <p>I currently work for a <a href="https://www.gorillagroup.com/" target="_blank" rel="noopener noreferrer">digital agency</a> out of Chicago, managing a team of creative technologists as we work to build complex e-commerce experiences for some of the world's largest clients.</p>
+    <p>I love all things JavaScript, semantic markup, accessibility and web performance.</p>
+    <p>My experience has ranged from building <a href="https://www.calendow.org/" rel="noopener noreferrer">intricate WordPress sites</a> to <a href="https://mobilising.salvationarmy.org/#!/">AngularJs community engagement portals</a> to spending nearly 2 years working with a team of engineers to build features, A/B tests and integrations using ReactJs on <a href="https://www.neighborhoods.com/" rel="noopener noreferrer">one of the most engaging real estate listing sites on the web</a>.</p>
     <p>When not helping to move the web forward by working on various exciting projects, I can be found cooking, hanging with my family, watching movies, riding my bike, seeking out good coffee or working out.</p>
     <p>Want to reach out? Send me a <a href="mailto:jon@bukiewi.cz">note</a>.</p>
   </Layout>
