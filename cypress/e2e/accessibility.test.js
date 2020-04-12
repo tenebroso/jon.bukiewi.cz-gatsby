4f98d7729c31573fb44040ee1a13e701f14d0c43
Jon Bukiewicz <jonathan@tenebroso.net>
2020-04-11 21:21:00 -0500

    Add cypress e2e and update some functionality and styling for mobile

---
+++ cypress/e2e/accessibility.test.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Accessibility tests", () => {
+  beforeEach(() => {
+    cy.visit("/")
+      .get("main")
+      .injectAxe()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.checkA11y()
+  })
+});
