f76966437ce9107d0a6cd4a8129190c6350e82b4
Jon Bukiewicz <jbukiewicz@g000805.lan>
2020-09-02 14:47:52 -0500

    Update bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ const IndexPage = () => (
     <p>Hey, there. Thanks for stopping by!</p>
     <p>My name is Jon Bukiewicz and I grew up building things on the web.</p>
     <p>I strive to find solutions by leveraging over 20 years of experience in front end development, project management, user-centric design, and clear communication.</p>
-    <p>I currently work for a <a href="https://www.gorillagroup.com/" target="_blank" rel="noopener noreferrer">digital agency</a> out of Chicago, managing a team of creative technologists as we work to build complex e-commerce experiences for some of the world's largest clients.</p>
+    <p>I currently work for a <a href="https://www.neighborhoods.com/" target="_blank" rel="noopener noreferrer">real estate software company</a> out of Chicago, working with a team of engineers to build performant and accessible features, A/B tests and integrations using React.</p>
     <p>I love all things JavaScript, semantic markup, accessibility and web performance.</p>
-    <p>My experience has ranged from building <a href="https://www.calendow.org/" rel="noopener noreferrer">intricate WordPress sites</a> to <a href="https://mobilising.salvationarmy.org/#!/">AngularJs community engagement portals</a> to spending nearly 2 years working with a team of engineers to build features, A/B tests and integrations using ReactJs on <a href="https://www.neighborhoods.com/" rel="noopener noreferrer">one of the most engaging real estate listing sites on the web</a>.</p>
-    <p>When not helping to move the web forward by working on various exciting projects, I can be found cooking, hanging with my family, watching movies, riding my bike, seeking out good coffee or working out.</p>
+    <p>My experience has ranged from building <a href="https://www.calendow.org/" rel="noopener noreferrer">intricate WordPress sites</a> to <a href="https://mobilising.salvationarmy.org/#!/">AngularJs community engagement portals</a> to e-commerce sites in Magento and Hybris for some of the largest brands in the world.</p>
+    <p>When not helping to move the web forward by working on various exciting projects, I can be found spending time with family, cooking, watching movies, biking, camping, seeking out good coffee or working out.</p>
     <p>Want to reach out? Send me a <a href="mailto:jon@bukiewi.cz">note</a>.</p>
   </Layout>
 );
