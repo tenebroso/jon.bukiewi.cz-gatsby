f35c370e33848463f4d393839ee486ef206a35dd
Jon Bukiewicz <jonathan@tenebroso.net>
2025-06-19 06:46:07 -0500

    Update index.js

--- src/components/SiteTitle/index.js
+++ src/components/SiteTitle/index.js
@@ -5,7 +5,7 @@ const SiteTitle = () => (
   <h1 className={styles.title}>
     Jon Bukiewicz &bull;&nbsp;
     <span className={styles.subTitle}>
-      Web Solutions Engineer&nbsp;
+      Engineering Manager&nbsp;
     </span>
     <span className={styles.location}>
       &bull; Milwaukee, Wisconsin
