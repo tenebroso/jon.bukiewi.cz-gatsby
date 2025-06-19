198b298638f7f0c68c474fea905d43458fbe8448
Jon Bukiewicz <jonathan@tenebroso.net>
2025-06-19 06:58:23 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Jon Bukiewicz',
+    title: 'Jon Bukiewicz | Remote Engineering Manager & Technical Leader',
     description: `Jon Bukiewicz is a remote Engineering Manager with a strong background in technical leadership, people management, and workplace equity-focused software. Currently leading high-impact engineering teams at Syndio.`,
     keywords: `Jon Bukiewicz, Engineering Manager, Software Engineering Leader, Technical Leadership, Remote Engineering, Workplace Equity, Syndio, PayEQ, PayFinder, Team Management, Product Development, Full Stack Engineering`,
     author: `@jonbukiewicz`,
